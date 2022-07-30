4ef94e20ee9877d34de17e6444f3f3f819a8b950
Alexis Brisse <brissealexis@gmail.com>
2022-07-30 20:47:35 +0100

    clean pipex

--- srcs/process.c
+++ srcs/process.c
@@ -6,7 +6,7 @@
 /*   By: abrisse <abrisse@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/30 18:16:44 by abrisse           #+#    #+#             */
-/*   Updated: 2022/07/30 19:26:59 by abrisse          ###   ########.fr       */
+/*   Updated: 2022/07/30 20:47:08 by abrisse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,17 +54,12 @@ static void	exec_cmd(char *cmd, char **env)
 
 void	child_process(int fd1, int *end, char *cmd, char **env)
 {
-	// add protection if dup2() < 0
 	close(end[0]);
-	dup2(fd1, STDIN_FILENO);	// Close STDIN, fd1 becomes the new STDIN.
-								// fd1 become execve() input.
-	dup2(end[1], STDOUT_FILENO);	// Close STDOUT, end[0] becomes the new
-									// STDOUT.
-									// end[0] becomes execve() output.
+	dup2(fd1, STDIN_FILENO);
+	dup2(end[1], STDOUT_FILENO);
 	close(end[1]);
 	close(fd1);
 	exec_cmd(cmd, env);
-//	if (WEXITSTATUS(status) == EXIT_FAILURE)
 	exit(EXIT_FAILURE);
 }
 
@@ -73,17 +68,11 @@ void	parent_process(int fd2, int *end, char *cmd, char **env)
 	int	status;
 
 	waitpid(-1, &status, 0);
-	// add protection if dup2() < 0
 	close(end[1]);
-	dup2(fd2, STDOUT_FILENO);	// Close STDOUT, fd2 becomes the new STDOUT.
-								// fd2 becomes execve() output.
-	dup2(end[0], STDIN_FILENO);	// Close STDIN, end[1] becomes the new STDIN.
-								// end[1] becomes execve() input.
-								// end[1] have end[0] value, who is the output 
-								// of the first command.
+	dup2(fd2, STDOUT_FILENO);
+	dup2(end[0], STDIN_FILENO);
 	close(end[0]);
 	close(fd2);
 	exec_cmd(cmd, env);
-//	if (WEXITSTATUS(status) == EXIT_FAILURE)
 	exit(EXIT_FAILURE);
 }
